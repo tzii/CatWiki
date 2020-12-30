4427ec3239be8d83013687a4f30cf7ea6baf3ab7
Vũ <tuanvundvn@gmail.com>
2020-12-30 10:30:16 +0700

    responsive topsearched page

--- src/components/CatInfo.tsx
+++ src/components/CatInfo.tsx
@@ -8,7 +8,7 @@ export default function CatInfo(props: { data: TopSearchedCat; index: number }) 
   return (
     <div className="mt-12 flex">
       <img
-        className="object-cover 2xl:w-48 2xl:h-48 xl:w-44 xl:h-44 lg:w-40 lg:h-40 md:w-36 md:h-36 sm:w-32 sm:h-32 w-28 h-28 rounded-2xl mr-5 cursor-pointer"
+        className="object-cover 2xl:w-48 2xl:h-48 xl:w-44 xl:h-44 lg:w-40 lg:h-40 md:w-36 md:h-36 sm:w-32 sm:h-32 w-24 h-24 rounded-2xl sm:mr-5 mr-3 cursor-pointer"
         src={props.data.image}
         alt=""
         onClick={() => history.push(props.data.id)}
